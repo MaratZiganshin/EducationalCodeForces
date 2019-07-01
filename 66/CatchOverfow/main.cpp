e4554680e94ee2f17c309a5ab5c6162adbced2fc
Marat Ziganshin <zig-marat@mail.ru>
2019-07-01 17:18:25 +0300

    66 b

--- 66/CatchOverfow/main.cpp
+++ 66/CatchOverfow/main.cpp
@@ -1,68 +1,57 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 #include <cmath>
 
 using namespace std;
 
-long long parseLines(vector<string>& lines, long long index)
+long long x;
+stack<long long> fors;
+
+bool parseLine(string& line)
 {
-    if (index >= lines.size())
-        return 0;
-    string line = lines[index];
     if (line == "add") {
-        long long result = parseLines(lines, index + 1) + 1;
-        if (result >= pow(2, 32))
+        x += fors.top();
+        if (x >= pow(2, 32))
         {
-            return -1;
+            return false;
         }
-        return result;
+        return true;
     }
     else if (line.substr(0, 3) == "for")
     {
         long long count = stoi(line.substr(4, line.size() - 4));
-        long long bal = 0;
-        long long i = index + 1;
-        while (lines[i] != "end" || bal != 0)
-        {
-            if (lines[i] == "end")
-                bal--;
-            else if (lines[i].substr(0, 3) == "for")
-                bal++;
-            i++;
-        }
-        long long r1 = parseLines(lines, index + 1);
-        if (r1 == -1 || r1 >= pow(2, 32))
-        {
-            return -1;
-        }
-        long long r2 = parseLines(lines, i + 1);
-        if (r2 == -1 || r2 >= pow(2, 32))
-        {
-            return -1;
-        }
-        return r1 * count + r2;
+        fors.push(min(fors.top() * count, (long long)pow(2, 32)));
+        return true;
     }
     else if (line == "end")
-        return 0;
+    {
+        fors.pop();
+        return true;
+    }
 }
 
 int main() {
+    x = 0;
     long long l;
     cin >> l;
+    fors.push(1);
 
-    vector<string> lines;
     string line;
     getline(cin, line);
+    bool succ = true;
     for (long long i = 0; i < l; i++)
     {
         getline(cin, line);
-        lines.push_back(line);
+        bool r = parseLine(line);
+        if (!r)
+        {
+            succ = false;
+        }
+
     }
 
-    long long r = parseLines(lines, 0);
-
-    if (r == -1 || r >= pow(2, 32))
+    if (!succ)
         cout << "OVERFLOW!!!";
     else
-        cout << r;
+        cout << x;
 }
